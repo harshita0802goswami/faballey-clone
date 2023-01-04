b83af9dacb8c7ce8109af5724fc691a1d66b597e
tanmoymandalofficial <tm.socialmidea@gmail.com>
2023-01-04 20:23:04 +0530

    slider complite

--- faballeyclone/src/App.js
+++ faballeyclone/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Products } from './Pages/Products/Products';
-
+import Home from './Pages/Home/Home';
 function App() {
   return (
     <div className="App">
-      <Products/>
+      {/* <Products/> */}
+      <Home />
     </div>
   );
 }
--- faballeyclone/src/Pages/Home/Home.jsx
+++ faballeyclone/src/Pages/Home/Home.jsx
@@ -1,5 +1,11 @@
+import Banner from "../../Components/Landing/Banner";
+
 const Home = ()=>{
-    return 
+    return (
+        <div>
+            <Banner />
+        </div>
+    )
 }
 
 export default Home;
