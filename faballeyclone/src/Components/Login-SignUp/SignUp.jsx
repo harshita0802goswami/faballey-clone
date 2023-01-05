d235ad0c03981498b37f779c21bbebff3aa54548
sulthan441 <mohammedsulthan441@gmail.com>
2023-01-05 15:23:29 +0530

    login complete

--- faballeyclone/src/Components/Login-SignUp/SignUp.jsx
+++ faballeyclone/src/Components/Login-SignUp/SignUp.jsx
@@ -5,86 +5,136 @@ import {
     Divider,
     Button,
     Text, Box, Flex, Image
-  } from "@chakra-ui/react";
-  import { CloseIcon } from "@chakra-ui/icons"
-  import { Icon } from '@chakra-ui/react'
-  import google from "./LoginImages/Login_Image1.png"
-  import facebook from "./LoginImages/Login_Image2.png"
-  import { useRef } from "react";
-  function MySignUp() {
-  
-  
+} from "@chakra-ui/react";
+import { CloseIcon } from "@chakra-ui/icons"
+import { Icon } from '@chakra-ui/react'
+import google from "./LoginImages/Login_Image1.png"
+import facebook from "./LoginImages/Login_Image2.png"
+import { useRef } from "react";
+import { useState } from "react";
+function MySignUp() {
+
+
+    //SignUp form handler state
+    const [input, setInput] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        
+    })
+
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    //close mark div ref
     const divRef = useRef();
-  
-  
+
+    //input border ref
     const inputRef = useRef()
     const inputRef1 = useRef()
     const inputRef2 = useRef()
+    const inputRef3 = useRef()
 
 
+    // close mark div function
     const closeDiv = () => {
-      divRef.current.style.display = divRef.current.style.display === 'none' ? 'block' : 'none';
-   // ADD NAVIGATE TO HOME PAGE HERE  
+        divRef.current.style.display = divRef.current.style.display === 'none' ? 'block' : 'none';
+        // ADD NAVIGATE TO HOME PAGE HERE  
     }
-  
+
+
+
+    //input border 
     const handleInputClick = () => {
-      inputRef.current.style.borderBottom = '2px solid black';
+        inputRef.current.style.borderBottom = '2px solid black';
     };
     const handleInputClick1 = () => {
         inputRef1.current.style.borderBottom = '2px solid black';
-      };
-      const handleInputClick2 = () => {
+    };
+    const handleInputClick2 = () => {
         inputRef2.current.style.borderBottom = '2px solid black';
-      };
+    };
+    const handleInputClick3 = () => {
+        inputRef3.current.style.borderBottom = '2px solid black';
+    };
+
+
+    // handle onchange function
+    const handleSignUp = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //handle submit 
+    const formSubmitHandler = (e) => {
+        e.preventDefault();
+        if (input.password === input.confirmPassword) {
+            setIsLoggedIn(true)
+            console.log(input);
+        }
+        if (input.password != input.confirmPassword) {
+            alert("Password does not match")
+        }
+
+
+    }
+
+
+
     return (
-  
-      <Box mt={'6%'} >
-          <Box  ref={divRef} bg={"white"} w={"35%"} boxShadow='md' borderRadius={"5px"} padding={"30px"} margin={"auto"} >
-            <Icon w={"12px"} as={CloseIcon} float={"right"} cursor={"pointer"} onClick={closeDiv} /> <br />
-            <Text ml={"5%"} paddingBottom={"33px"} textAlign={"center"} fontFamily={"Lato"} fontSize={"24px"} fontWeight={"bold"}> REGISTER HERE </Text>
-            <Text ml={"5%"} paddingBottom={"19px"} textAlign={"left"} fontFamily={"Lato"} fontSize={"14px"} fontWeight={"bold"}> for a quicker checkout</Text>
-  
-            <FormControl isRequired >
-               <FormLabel ml={"5%"}>Email</FormLabel>
-              <Input borderRadius={"none"} ref={inputRef1}  onClick={handleInputClick1} w={"90%"} padding={"27px"} type="email" id="email" placeholder="ENTER EMAIL ADRESS" mb={"24px"}/>
-              <FormLabel ml={"5%"}>Enter Password</FormLabel>
 
-              <Input borderRadius={"none"} ref={inputRef2}  onClick={handleInputClick2} w={"90%"} padding={"27px"} type="password" id="password" placeholder="ENTER PASSWORD" />
-             <FormLabel ml={"5%"} mt={"24px"} > Confirm Password</FormLabel>
-              <Input borderRadius={"none"} ref={inputRef}  onClick={handleInputClick} w={"90%"} padding={"27px"} type="password" id="password" placeholder="CONFIRM PASSWORD" />
-            </FormControl>
-            <Button borderRadius={"0px"} w={"90%"} color={"white"} padding={"27px"} marginTop={"35px"} marginBottom={"30px"} type="submit" colorScheme='red' bg={"#fc6486"}>
-              CONTINUE
-            </Button>
-  
-            <Flex align="center" mb={"30px"} fontWeight={"thin"}>
-              <Divider />
-              <Text opecity fontFamily={"Lato"} fontSize={"13px"} opacity={"0.8"} >Or_continue_with</Text>
-              <Divider />
-            </Flex >
-  
-  
-            <Box margin={"auto"} justifyContent={"center"} gap={"8px"} w="80%" display={"flex"}>
-              <Image cursor={"pointer"} w={"42%"} src={google} alt={"google"} />
-              <Image cursor={"pointer"} w={"42%"} src={facebook} alt={"facebook"} />
-  
-  
+        <Box>
+            <Box ref={divRef} bg={"white"} w={"35%"} boxShadow='md' borderRadius={"5px"} padding={"30px"} margin={"auto"} >
+                <Icon w={"12px"} as={CloseIcon} float={"right"} cursor={"pointer"} onClick={closeDiv} /> <br />
+                <Text ml={"5%"} paddingBottom={"33px"} textAlign={"center"} fontFamily={"Lato"} fontSize={"24px"} fontWeight={"bold"}> REGISTER HERE </Text>
+                <FormControl isRequired >
+
+                    <FormLabel ml={"5%"}>Name</FormLabel>
+                    <Input name={"name"} onChange={handleSignUp} borderRadius={"none"} ref={inputRef3} onClick={handleInputClick3} w={"90%"} padding={"27px"} type="email" id="email" placeholder="ENTER EMAIL ADRESS" mb={"24px"} />
+
+
+                    <FormLabel ml={"5%"}>Email</FormLabel>
+                    <Input name={"email"} onChange={handleSignUp} borderRadius={"none"} ref={inputRef1} onClick={handleInputClick1} w={"90%"} padding={"27px"} type="email" id="email" placeholder="ENTER EMAIL ADRESS" mb={"24px"} />
+                    <FormLabel ml={"5%"}>Enter Password</FormLabel>
+                    <Input name={"password"} onChange={handleSignUp} borderRadius={"none"} ref={inputRef2} onClick={handleInputClick2} w={"90%"} padding={"27px"} type="password" id="password" placeholder="ENTER PASSWORD" />
+                    <FormLabel ml={"5%"} mt={"24px"} > Confirm Password</FormLabel>
+                    <Input name={"confirmPassword"} onChange={handleSignUp} borderRadius={"none"} ref={inputRef} onClick={handleInputClick} w={"90%"} padding={"27px"} type="password" id="password" placeholder="CONFIRM PASSWORD" />
+                </FormControl>
+                <Button onClick={formSubmitHandler} borderRadius={"0px"} w={"90%"} color={"white"} padding={"27px"} marginTop={"35px"} marginBottom={"30px"} type="submit" colorScheme='red' bg={"#fc6486"}>
+                    CONTINUE
+                </Button>
+
+                <Flex align="center" mb={"30px"} fontWeight={"thin"}>
+                    <Divider />
+                    <Text opecity fontFamily={"Lato"} fontSize={"13px"} opacity={"0.8"} >Or_continue_with</Text>
+                    <Divider />
+                </Flex >
+
+
+                <Box margin={"auto"} justifyContent={"center"} gap={"8px"} w="80%" display={"flex"}>
+                    <Image cursor={"pointer"} w={"42%"} src={google} alt={"google"} />
+                    <Image cursor={"pointer"} w={"42%"} src={facebook} alt={"facebook"} />
+
+
+                </Box>
+
+                <Box>
+                    <Text mt={"24px"} color={"blue"} textDecoration={"underline"} opecity fontFamily={"Lato"} fontSize={"13px"} opacity={"0.8"} cursor={"pointer"} >Already Registered? Login</Text>
+                </Box>
+
+
+
+                <Text opacity={"0.6"}
+                    cursor={"pointer"} fontWeight={"semibold"} mt={"30px"} fontSize={"13px"} onClick={closeDiv} ref={divRef} >Skip</Text>
+
+
             </Box>
 
-            <Box>
-              <Text mt={"24px"} color={"blue"} textDecoration={"underline"} opecity fontFamily={"Lato"} fontSize={"13px"} opacity={"0.8"} cursor={"pointer"} >Already Registered? Login</Text>
-            </Box>
-  
-  
-  
-            <Text opacity={"0.6"}
-              cursor={"pointer"} fontWeight={"semibold"} mt={"30px"} fontSize={"13px"} onClick = {closeDiv} ref={divRef} >Skip</Text>
-  
-  
-          </Box> 
-  
-      </Box>
-  
+        </Box>
+
     );
-  }
-  export default MySignUp
+}
+export default MySignUp
