49f3af9c59debe5fb5c5c37750f959b722996af1
nishantp15 <prajapati.nishant@outlook.com>
2023-01-05 19:18:09 +0530

    cart rev03

--- faballeyclone/src/Components/CartAndCheckout/Loading.jsx
+++ faballeyclone/src/Components/CartAndCheckout/Loading.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import { Spinner } from '@chakra-ui/react'
 const Loading = () => {
   return (
-    <div>Loading...</div>
+    <div style={{display:"flex", justifyContent:"center", alignItems:"center",height:"400px"}}><Spinner
+    thickness='4px'
+    speed='0.65s'
+    emptyColor='gray.200'
+    color='blue.500'
+    size='xl'
+  /></div>
   )
 }
 
