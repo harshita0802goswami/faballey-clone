e435e9f2a90f4c585af0f53748a91f59253ad139
nishantp15 <prajapati.nishant@outlook.com>
2023-01-06 14:46:28 +0530

    cart rev08

---
+++ faballeyclone/src/Components/CartAndCheckout/CheckoutPaymentDetails.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from '../../Pages/Cart/Checkout.module.css'
+import { useState, useEffect } from 'react';
+function CheckoutPaymentDetails() {
+    let [PaymentToMake, setPaymentToMake] = useState({});
+    useEffect(() => {
+        let PaymentDetails = JSON.parse(localStorage.getItem("PaymentDetails"));
+        setPaymentToMake(PaymentDetails);
+      }, []);
+
+  return (
+    <>
+          <div className={styles.PaymentDetailsSubDiv}>
+            <div>
+              <div>
+                <p>Sub Total</p>
+                <p>₹ {PaymentToMake.SubTotal}</p>
+              </div>
+              <div style={{ color: "rgb(252, 100, 134)" }}>
+                <p>Discount</p>
+                <p>-₹ {PaymentToMake.Discount}</p>
+              </div>
+              <div>
+                <p>Donation</p>
+                <p>₹ {PaymentToMake.Donated}</p>
+              </div>
+            </div>
+            <div className={styles.CheckoutTotal}>
+              <p>Total</p>
+              <p>₹ {PaymentToMake.Total}</p>
+            </div>
+          </div>
+    </>
+  )
+}
+
+export default CheckoutPaymentDetails
