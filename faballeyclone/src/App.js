bf87b9c07165eb019b93f69b662184f01c43c7e3
sulthan441 <mohammedsulthan441@gmail.com>
2023-01-06 22:36:37 +0530

    ok

--- faballeyclone/src/App.js
+++ faballeyclone/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Home from './Pages/Home/Home';
 import { Products } from './Pages/Products/Products';
-
+import MyLogin from './Components/Login-SignUp/Login';
+import MySignUp from "./Components/Login-SignUp/SignUp"
 function App() {
   return (
     <div className='app'>
       <Products/>
       <Home/>
+      <MyLogin/>
+      <MySignUp/>
+      
     </div>
   );
 }
