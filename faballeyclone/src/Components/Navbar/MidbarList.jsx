84d630a4bf422ce514fa7e0882a9fb6b7ba44297
tanmoymandalofficial <tm.socialmidea@gmail.com>
2023-01-07 15:06:31 +0530

    navbar

--- faballeyclone/src/Components/Navbar/MidbarList.jsx
+++ faballeyclone/src/Components/Navbar/MidbarList.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
+import './Navbar.css';
+import MidbarCard from './MidbarCard';
 
 const MidbarList = () => {
   return (
     <div>
-        <ul>
-            <li>NEW IN</li>
-            <li>NEW INRIMZIM DADU X FABALLEY</li>
-            <li>WINTERWEAR</li>
-            <li>CLOTHING</li>   
-            <li>TOPS</li>
+        <ul className='navList'>
+            <li>NEW IN
+              <MidbarCard 
+               list={['Clothing', 'Curve', 'Accessories', 'Bestselling']}
+               img='https://img.faballey.com/images/menu/newinsmenuimg1.jpg'
+              />
+            </li>
+            <li>NEW INRIMZIM DADU X FABALLEY
+              <MidbarCard 
+               list={['Shop The Collection', 'Explore The Lookbook']}
+               img='https://img.faballey.com/images/menu/aw22rdmenuimg1.jpg'
+              />
+            </li>
+            <li>WINTERWEAR </li>
+            <li>CLOTHING 
+              <MidbarCard 
+               list={['Dresses', 'Co-ords','Skirts','Bottoms','Basics','Classics','Workwear','Loungewear','Winterwear']}
+               list2={['Party', 'Vacation','Work','Loungewear']}
+               h2='SHOP BY OCCASION'
+              />
+            </li>   
+            <li>TOPS
+            <MidbarCard 
+               list={['Crop Tops', 'Tank Tops','T-Shirts','Shirts','Basics','Blouses','Maxi Tops']}
+               list2={['Off Shoulder Tops', 'Cold Shoulder Tops','One Shoulder Tops']}
+               img='https://img.faballey.com/images/menu/topmenuimg1.jpg'
+               h1='SHOP BY STYLE'
+              />
+            </li>
             <li>DRESSES</li>
             <li>CO-ORDS</li>
             <li>PARTYWEAR</li>
@@ -23,3 +48,15 @@ const MidbarList = () => {
 export default MidbarList
 
 //
+
+
+
+
+
+
+
+
+// 
+// 
+// 
+// 
